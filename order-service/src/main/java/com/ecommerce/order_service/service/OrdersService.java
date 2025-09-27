51dca1d33fa4f2fbc47c9083d7cdc977f205d022
shah8375033 <shah8375033@gmail.com>
2025-09-27 05:36:50 +0530

    Implementing resilience4j

--- order-service/src/main/java/com/ecommerce/order_service/service/OrdersService.java
+++ order-service/src/main/java/com/ecommerce/order_service/service/OrdersService.java
@@ -6,6 +6,9 @@ import com.ecommerce.order_service.entity.OrderItem;
 import com.ecommerce.order_service.entity.OrderStatus;
 import com.ecommerce.order_service.entity.Orders;
 import com.ecommerce.order_service.repository.OrdersRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -37,7 +40,9 @@ public class OrdersService {
         return modelMapper.map(order, OrderRequestDto.class);
     }
 
-
+    @Retry(name = "inventoryRetry",fallbackMethod = "createOrderFallback")
+    @RateLimiter(name = "inventoryRateLimiter",fallbackMethod = "createOrderFallback")
+    @CircuitBreaker(name = "inventoryCircuitBreaker",fallbackMethod ="createOrderFallback")
     public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
         Double totalPrice=inventoryFeignClient.reduceStocks(orderRequestDto);
         Orders orders=modelMapper.map(orderRequestDto,Orders.class);
@@ -48,5 +53,10 @@ public class OrdersService {
         orders.setOrderStatus(OrderStatus.Confirmed);
         Orders savedOrder=ordersRepository.save(orders);
         return modelMapper.map(savedOrder, OrderRequestDto.class);
+    }
+    public OrderRequestDto createOrderFallback(OrderRequestDto orderRequestDto, Throwable throwable) {
+        log.error("Fallback Method occured due to {}"+throwable.getMessage());
+        return new OrderRequestDto();
+
     }
 }
